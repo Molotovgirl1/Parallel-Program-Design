b46865ee0f3b03804e9584e237926b811e220fa2
ybx <2110508@mail.nankai.edu.cn>
2024-05-13 21:36:12 +0800

    静态线程+信号量同步+三重循环全部纳入线程函数+按列划分任务

--- lab3_Pthread和Openmp/pthread四种按列划分算法/pthread3_Col.cpp
+++ lab3_Pthread和Openmp/pthread四种按列划分算法/pthread3_Col.cpp
@@ -129,7 +129,7 @@ int main() {
 		QueryPerformanceCounter((LARGE_INTEGER*)&end);
 		timeuse += (end - begin) * 1000.0 / freq;
 	}
-	cout << "n=" << N << " pthread3: " << timeuse / times << "ms" << endl;
+	cout << "n=" << N << " pthread3_Col: " << timeuse / times << "ms" << endl;
 
 	return 0;
 }
